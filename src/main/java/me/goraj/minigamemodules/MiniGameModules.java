95d1cbc18f2fd1ea40c8e47158143fe027aa38c1
MrGoraj <32809530+MrGoraj@users.noreply.github.com>
2023-01-30 17:19:03 +0100

    initial commit

---
+++ src/main/java/me/goraj/minigamemodules/MiniGameModules.java
@@ -0,0 +1,17 @@
+package me.goraj.minigamemodules;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiniGameModules extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
